b25ba201d2e4a35e94a285eebec716c9609cc398
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-03-13 14:10:23 -0300

    commit inicial

---
+++ socifras/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Genero, VideoAulas
+
+admin.site.register(Genero)
+admin.site.register(VideoAulas)
+
+# Register your models here.
+
---
+++ socifras/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Genero(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.nome
+    
+class VideoAulas(models.Model):
+    descricao = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def _str_(self):
+        return self.nome
+# Create your models here.
