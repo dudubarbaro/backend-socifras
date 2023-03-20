6b837200578dfed2fe4f529d21be4b20e93395a1
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-03-20 14:11:31 -0300

    error database e relacionamento artista musica

--- socifras/models.py
+++ socifras/models.py
@@ -26,19 +26,19 @@ class Artista(models.Model):
 
 class Musica(models.Model):
     titulo = models.CharField(max_length=255)
-    artista = models.CharField(max_length=255)
     tom = models.CharField(max_length=10)
     videoaula = models.URLField(null= True, blank= True)
 
-    categoria = models.ForeignKey(
+    generos = models.ForeignKey(
         Genero, on_delete=models.PROTECT, related_name="musicas"
     )
-    categoria = models.ForeignKey(
+    videoaulas = models.ForeignKey(
         VideoAulas, on_delete=models.PROTECT, related_name="musicas"
     )
-    categoria = models.ForeignKey(
-        Artista, on_delete=models.PROTECT, related_name="musicas"
+    artistas = models.ManyToManyField(
+        Artista, related_name="musicas"
     )
+    
 
     def _str_(self):
         return f"{self.titulo} ({self.artista})"
